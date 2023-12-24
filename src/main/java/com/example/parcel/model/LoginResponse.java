6549fc5b39ba00efb8735ff4e46d6c560685ccc4
selim <lawalselim0@gmail.com>
2023-12-24 13:14:29 +0000

    JWT login response body

---
+++ src/main/java/com/example/parcel/model/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.example.parcel.model;
+
+public class LoginResponse {
+    /** The JWT token to be used for authentication. */
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
--- src/main/java/com/example/parcel/repository/UserRepository.java
+++ src/main/java/com/example/parcel/repository/UserRepository.java
@@ -5,7 +5,7 @@ import com.example.parcel.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface                                                      UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
     User findUserById(Long userId);
     //add other custom queries here
